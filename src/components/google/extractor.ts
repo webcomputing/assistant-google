f340fc93b3ca762a9652a75435f1ef53802a0438
Antonius Ostermann <ostermann.antonius@gmail.com>
2017-10-27 15:46:19 +0200

    improves google handle by adding additional features such as cards and suggestion chips

--- src/components/google/extractor.ts
+++ src/components/google/extractor.ts
@@ -40,14 +40,14 @@ export class Extractor extends ApiAiExtractor implements unifierInterfaces.Reque
 
   protected getOAuthToken(context: rootInterfaces.RequestContext): string | null {
     if (typeof context.body.originalRequest.data !== "undefined" && typeof context.body.originalRequest.data.user !== "undefined")
-      return context.body.originalRequest.data.user.access_token;
+      return context.body.originalRequest.data.user.accessToken;
     else
       return null;
   }
 
   protected getTemporalToken(context: rootInterfaces.RequestContext): string | null {
     if (typeof context.body.originalRequest.data !== "undefined" && typeof context.body.originalRequest.data.user !== "undefined")
-      return context.body.originalRequest.data.user.user_id;
+      return context.body.originalRequest.data.user.userId;
     else
       return null;
   }
