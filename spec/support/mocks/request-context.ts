d44e779f8486d1b6c685d9cd8f224e5d8f1a1130
dennis <de@web-computing.de>
2018-07-25 09:40:44 +0200

    Fix specs

--- spec/support/mocks/request-context.ts
+++ spec/support/mocks/request-context.ts
@@ -7,18 +7,75 @@ export const validRequestContext: RequestContext = {
   method: "POST",
   headers: {},
   body: {
-    originalRequest: {
-      data: {
-        surface: { capabilities: [{ name: "actions.capability.AUDIO_OUTPUT" }, { name: "actions.capability.SCREEN_OUTPUT" }] },
-        inputs: [{ rawInputs: [{ inputType: "VOICE", query: "My query" }], intent: "actions.intent.MAIN" }],
-        user: { locale: "de-DE", userId: "my-user-id", accessToken: "my-access-token", userStorage: "{\"my-session-key\":\"my-session-value\"}"},
-        conversation: { conversationId: "1509984249465", type: "NEW" },
-        availableSurfaces: [{ capabilities: [{ name: "actions.capability.AUDIO_OUTPUT" }, { name: "actions.capability.SCREEN_OUTPUT" }] }],
+    responseId: "my-dialogflow-response-id",
+    session: "my-dialogflow-session",
+    queryResult: {
+      queryText: "user's original agent query",
+      languageCode: "en",
+      parameters: {},
+      allRequiredParamsPresent: true,
+      fulfillmentMessages: [
+        {
+          text: {
+            text: [""],
+          },
+        },
+      ],
+      intent: {
+        name: "my-unique-dialogflow-intent-name",
+        displayName: "testIntent",
       },
+      intentDetectionConfidence: 1,
     },
-    sessionId: "my-session-id",
-    timestamp: "2017-06-24T16:00:18Z",
-    result: { resolvedQuery: "My query", metadata: { intentName: "testIntent" } },
-    lang: "en",
+    originalDetectIntentRequest: {
+      source: "google",
+      version: "2",
+      payload: {
+        isInSandbox: true,
+        surface: {
+          capabilities: [
+            {
+              name: "actions.capability.SCREEN_OUTPUT",
+            },
+          ],
+        },
+        inputs: [
+          {
+            rawInputs: [
+              {
+                query: "Talk to my test app",
+                inputType: "VOICE",
+              },
+            ],
+            intent: "actions.intent.MAIN",
+          },
+        ],
+        user: {
+          lastSeen: "2018-03-16T22:08:48Z",
+          permissions: ["UPDATE"],
+          locale: "en-US",
+          idToken: "my-google-user-id",
+          userStorage: '{"my-session-key":"my-session-value"}',
+          accessToken: "my-access-token",
+        },
+        accessToken: "my-access-token",
+      },
+      conversation: {
+        conversationId: "1509984249465",
+        type: "NEW",
+      },
+      availableSurfaces: [
+        {
+          capabilities: [
+            {
+              name: "actions.capability.SCREEN_OUTPUT",
+            },
+            {
+              name: "actions.capability.AUDIO_OUTPUT",
+            },
+          ],
+        },
+      ],
+    },
   },
 };
